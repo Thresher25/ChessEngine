7c32c1bbe6c0ee08938c98ea5bf459af91c4caa0
Thresher25 <JacobPerzan@gmail.com>
2018-02-07 17:39:23 -0500

    Setting up project

---
+++ src/Main/MainClass.java
@@ -0,0 +1,13 @@
+package Main;
+
+import javax.swing.*;
+
+public class MainClass extends JPanel{
+
+
+    public static void main(String...args){
+        System.out.println("Not done yet ;)");
+    }
+
+
+}
---
+++ src/Pieces/ChessPiece.java
@@ -0,0 +1,7 @@
+package Pieces;
+
+public abstract class ChessPiece {
+
+
+
+}
